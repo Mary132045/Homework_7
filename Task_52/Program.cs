d5fe20cb63becb76af71b2e38a10d35e3961cd38
Mary132045 <138201784+Mary132045@users.noreply.github.com>
2023-07-27 12:25:15 +0300

    Update Program.cs

--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -3,38 +3,38 @@ Console.Write("Введите количество строк массива: ")
 int i = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите количество столбцов массива: ");
 int j = Convert.ToInt32(Console.ReadLine());
-int[,] randomArray = new int[i,j];
+int[,] randomArray = new int[i, j];
 
 void nums(int i, int j)
 {
-int k,l;
-Random rand = new Random();
-for (k = 0; k < i; k++)
-{
-Console.WriteLine();
-for (l = 0; l < j; l++)
-{
-randomArray[k,l] = rand.Next(1,9);
-Console.Write($"{randomArray[k,l]} ");
-}
-Console.WriteLine();
-}
+    int k, l;
+    Random rand = new Random();
+    for (k = 0; k < i; k++)
+    {
+        Console.WriteLine();
+        for (l = 0; l < j; l++)
+        {
+            randomArray[k, l] = rand.Next(1, 9);
+            Console.Write($"{randomArray[k, l]} ");
+        }
+        Console.WriteLine();
+    }
 }
 
 void avg(int i, int j)
 {
-Console.Write("Среднее арифметическое по столбцам массива: ");
-int k,l;
-Random rand = new Random();
-for (l = 0; l < j; l++)
-{
-double sum = 0;
-for (k = 0; k < i; k++)
-{
-sum = sum + randomArray[k,l];
+    Console.Write("Среднее арифметическое по столбцам массива: ");
+    int k, l;
+    Random rand = new Random();
+    for (l = 0; l < j; l++)
+    {
+        double sum = 0;
+        for (k = 0; k < i; k++)
+        {
+            sum = sum + randomArray[k, l];
+        }
+        Console.Write($"{sum / (k):F2}  ");
+    }
 }
-Console.Write($"{sum/(k):F2}  ");
-}
-}
-nums(i,j);
-avg(i,j);
+nums(i, j);
+avg(i, j);
